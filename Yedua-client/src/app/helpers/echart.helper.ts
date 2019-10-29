61c880031963ced351ea70266527013ea99c0c92
Rowad <rowadwo@gmail.com>
2019-10-30 00:01:59 +0200

    not bad looking tree

--- Yedua-client/src/app/helpers/echart.helper.ts
+++ Yedua-client/src/app/helpers/echart.helper.ts
@@ -4,7 +4,8 @@ import { DeepPartial } from 'utility-types';
 export const TREE_OPTION: DeepPartial<EChartOption> = {
   tooltip: {
     trigger: 'item',
-    triggerOn: 'mousemove'
+    triggerOn: 'mousemove',
+    formatter: ({ data: { name, value } }: any) => `${name}: children: ${value}`
   },
 
   legend: {
@@ -15,59 +16,48 @@ export const TREE_OPTION: DeepPartial<EChartOption> = {
       {
         name: 'tree1',
         icon: 'rectangle'
-      },
-      {
-        name: 'tree2',
-        icon: 'rectangle'
       }
     ]
   },
   series: [
     {
       type: 'tree',
-      name: 'tree1',
-      data: [],
-      // top: '1%',
-      // left: '7%',
-      // bottom: '1%',
-      // right: '20%',
-      top: '1%',
-      left: '15%',
-      bottom: '-7%',
-      right: '7%',
-      initialTreeDepth: -1, // expand all...
-      symbolSize: 10,
-      symbolBoundingData: 20,
-      symbolPatternSize: 20,
-      // symbol: 'roundRect',
-      lineStyle: {
-        width: 5,
-        shadowColor: 'rgba(0, 0, 0, 0.5)',
-        shadowBlur: 10,
-        opacity: 0.1
-      },
 
+      top: '1%',
+      left: '7%',
+      bottom: '1%',
+      right: '20%',
+
+      symbolSize: 10,
+
+      lineStyle: {
+        color: '#191F2B',
+        opacity: 0.5,
+        shadowBlur: 5,
+        shadowColor: '#F2F2F2',
+        curveness: 0.4
+      },
       label: {
         normal: {
-          // position: 'right',
-          // verticalAlign: 'middle',
-          // align: 'left',
-          fontSize: 10
-          // padding: 100,
-          // color: '#fff'
+          position: 'left',
+          verticalAlign: 'middle',
+          align: 'right',
+          fontSize: 20,
+          color: '#F2F2F2'
         }
       },
-
+      initialTreeDepth: -1,
+      itemStyle: {
+        borderColor: 'white'
+      },
       leaves: {
         label: {
           // normal: {
           position: 'right',
           verticalAlign: 'middle',
           align: 'left',
-          fontSize: 10,
-          padding: 100,
-          color: '#fff'
-          // } as any
+          color: '#F2F2F2'
+          // }
         }
       },
 
